c399c291b950cb07ad7f32e111c8cb3ef8ad8711
Nate Anderson <nathanaelcanderson@gmail.com>
2017-02-06 19:15:53 -0500

    finish tut-02

--- src/main/java/nate/anderson/game/AppletMain.java
+++ src/main/java/nate/anderson/game/AppletMain.java
@@ -1,27 +1,52 @@
 package nate.anderson.game;
 
 import java.applet.Applet;
-import asciiPanel.AsciiPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class AppletMain extends Applet {
+import asciiPanel.AsciiPanel;
+import nate.anderson.screen.Screen;
+import nate.anderson.screen.StartScreen;
+
+public class AppletMain extends Applet implements KeyListener {
     private static final long serialVersionUID = 2560255315130084198L;
 
     private AsciiPanel terminal;
-
+    private Screen screen;
+    
     public AppletMain(){
         super();
         terminal = new AsciiPanel();
-        terminal.write("rl tutorial", 1, 1);
         add(terminal);
+        screen = new StartScreen();
+        addKeyListener(this);
+        repaint();
     }
 
+    @Override
     public void init(){
         super.init();
         this.setSize(terminal.getWidth() + 20, terminal.getHeight() + 20);
     }
 
+    @Override
     public void repaint(){
+    	terminal.clear();
+    	screen.displayOutput(terminal);
         super.repaint();
-        terminal.repaint();
     }
+    
+	public void keyReleased(KeyEvent e) { 
+		
+	}
+
+	public void keyTyped(KeyEvent e) { 
+		
+	}
+    
+    public void keyPressed(KeyEvent e) {
+    	screen = screen.respondToUserInput(e);
+    }
+    
+    
 }
--- src/main/java/nate/anderson/game/ApplicationMain.java
+++ src/main/java/nate/anderson/game/ApplicationMain.java
@@ -1,25 +1,53 @@
 package nate.anderson.game;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import asciiPanel.AsciiPanel;
+import nate.anderson.screen.Screen;
+import nate.anderson.screen.StartScreen;
 
-public class ApplicationMain extends JFrame {
+public class ApplicationMain extends JFrame implements KeyListener {
 	
     private static final long serialVersionUID = 1060623638149583738L;
 
     private AsciiPanel terminal;
+    private Screen screen;
 
     public ApplicationMain(){
         super();
         terminal = new AsciiPanel();
-        terminal.write("rl tutorial", 1, 1);
         add(terminal);
         pack();
+        screen = new StartScreen();
+        addKeyListener(this);
+        repaint();
     }
-
+    
+    public void repaint() {
+    	terminal.clear();
+    	screen.displayOutput(terminal);
+    	super.repaint();
+    }
+    
+    public void keyPressed(KeyEvent e) {
+    	screen = screen.respondToUserInput(e);
+    	repaint();
+    }
+    
+    public void keyReleased(KeyEvent e) {
+		
+	}
+    
+    public void keyTyped(KeyEvent e) {
+  
+    }
+    
     public static void main(String[] args) {
         ApplicationMain app = new ApplicationMain();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
     }
+
 }
